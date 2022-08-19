b1424c81fefd1b2f1e80d69b8deb8c46e6b9fc5a
bhuvana <rbhuvanareddy@gmail.com>
2022-08-19 23:12:26 +0530

    bhuvana

---
+++ 18 aug/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    internal class Program
+    {
+        private static object emptyornull;
+
+        static void Main(string[] args)
+        {
+            //15
+            //Console.WriteLine("enter the us dollers");
+            //int dollers = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("dollers converted to indian rupees ={0}", (dollers*69));
+
+
+
+
+
+
+
+            //14
+            //Console.WriteLine("enter the temperature in celsius");
+            //int celsius = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("celsius converted to fahrenheit ={0}", (celsius * 9) / 5 + 32);
+
+
+
+
+            //13
+            //string s = string.Empty;
+            //if (string.IsNullOrEmpty(s)) ;
+            //{
+            //    Console.WriteLine("string is either null or empty");
+            //}
+
+
+
+
+
+            //10,11
+            //Console.WriteLine("\nInput first integer:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input second integer:");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Check if one is negative and one is positive:");
+            //Console.WriteLine((x < 0 || y > 0) && (x > 0 || y < 0));
+
+
+
+
+
+            //9
+            //Console.WriteLine("enter the celsius");
+            //int celsius = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("kelvin={0}",celsius+273);
+            //Console.WriteLine("fahrenheit={0}",celsius*18/10+32);
+
+
+
+
+
+
+            //8
+            //int i;
+            //Console.WriteLine("enter a number");
+            //i = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("{0} {0} {0}",i);
+            //Console.WriteLine("{0} {0}", i);
+            //Console.WriteLine("{0} {0}", i);
+            //Console.WriteLine("{0} {0}", i);
+            //Console.WriteLine("{0} {0} {0}", i);
+
+
+            int i;
+            Console.WriteLine("enter a number");
+            i = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} {0} {0} {0}",i);
+            Console.WriteLine("{0} {0} {0} {0}", i);
+            Console.WriteLine("{0} {0} {0} {0}", i);
+            Console.WriteLine("{0} {0} {0} {0}", i);
+            Console.WriteLine("{0} {0} {0} {0}", i);
+
+
+
+
+
+
+            //6
+            //int age;
+            //Console.Write("Enter your age ");
+            //age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("You look younger than {0} ", age);
+
+
+
+
+
+            //5
+            //int number1, number2, number3;
+
+            //Console.Write("Enter first number - ");
+            //number1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter second number - ");
+            //number2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter third number - ");
+            //number3 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Result of specified numbers {0}, {1} and {2}, (x+y)·z is {3} and x·y + y·z is {4}\n\n",
+            //    number1, number2, number3, ((number1 + number2) * number3), (number1 * number2 + number2 * number3));
+
+
+
+
+
+
+            //4
+
+            //   double number1, number2, number3, number4;
+
+            //   Console.Write("Enter the First number: ");
+            //   number1 = Convert.ToDouble(Console.ReadLine());
+
+            //   Console.Write("Enter the Second number: ");
+            //   number2 = Convert.ToDouble(Console.ReadLine());
+
+            //   Console.Write("Enter the third number: ");
+            //   number3 = Convert.ToDouble(Console.ReadLine());
+
+            //   Console.Write("Enter the fourth number: ");
+            //   number4 = Convert.ToDouble(Console.ReadLine());
+
+            //   double result = (number1 + number2 + number3 + number4) / 4;
+            //   Console.WriteLine("The average of {0}, {1}, {2}, {3} is: {4} ",
+            //number1, number2, number3, number4, result);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+           //3
+
+            //Console.Write("Enter a number: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter another number: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            //Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+            //Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2);
+            //Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+            //Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1 % num2);
+            
+        }
+        
+        
+        
+        
+        
+        
+        //2
+        //int num1, num2, num3;
+
+        //Console.Write("Input the first number: ");
+        //num1 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.Write("Input the second number: ");
+        //num2 = Convert.ToInt32(Console.ReadLine());
+
+        //Console.Write("Input the third number : ");
+        //num3 = Convert.ToInt32(Console.ReadLine());
+
+        //int result = num1 * num2 * num3;
+        //Console.WriteLine("Output: {0} x {1} x {2} = {3}",
+        //                    num1, num2, num3, result);
+       
+
+
+
+
+
+
+
+
+       // problem1
+        //int n1, n2;
+        //Console.WriteLine("enter the first number");
+        //n1 = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("enter second number");
+        //n2 = Convert.ToInt32(Console.ReadLine());
+        //exchange(ref n1, ref n2);
+        //Console.WriteLine("first number after exchange =");
+        //Console.WriteLine("second number after exchange =");
+    }
+
+        
+    }
+
+        
+           
+    
+
