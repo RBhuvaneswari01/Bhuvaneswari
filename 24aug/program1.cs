2771c5dbc4d8795c61153a9ed27f2840ccce36e2
bhuvana <rbhuvanareddy@gmail.com>
2022-08-25 13:44:49 +0530

    upload

---
+++ 24aug/program1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reusablecodelibrary
+{
+    internal class program1
+    {
+        public static void CompareString()
+        {
+
+            string str1 = "bhuvana";
+            string str2 = "ramasani";
+            if (String.Compare(str1, str2) == 0)
+            {
+
+                Console.WriteLine("str1 and str2 are equal");
+            }
+
+            else
+            {
+                Console.WriteLine("str1 and str2 are not equal.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 24aug/program6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reusablecodelibrary
+{
+    internal class program6
+    {
+        public static void creatingfile()
+        {
+            string path = @"D\c#\basic codes\ConsoleApp1\day 7firstfile.txt";
+            File.Create(path);
+        }
+
+    }
+}
