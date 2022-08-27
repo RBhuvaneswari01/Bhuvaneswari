09bc4fd5de696b3195a2d1680b8bd8f42b4cba94
bhuvana <rbhuvanareddy@gmail.com>
2022-08-27 12:38:26 +0530

    upload

---
+++ 26aug/aa4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9
+{
+  
+    internal class aa4
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddFirst("bhuvana");
+            list.AddLast("mamatha");
+            list.AddAfter(list.First, "hema");
+            list.AddBefore(list.Last, "koushik");
+            list.AddBefore(list.Last.Previous, "shanvika");
+            list.AddAfter(list.First.Next, "minnu");
+            list.Find("hema");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 26aug/program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace day9
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] S = { new Student(), new Student() };
+
+            S[0].SetStudent(101, "bhuvana", 7000);
+            S[1].SetStudent(102, "navya", 10000);
+
+            S[0].PrintStudent();
+            S[1].PrintStudent();
+            Console.ReadLine();
+        }
+    }
+}
