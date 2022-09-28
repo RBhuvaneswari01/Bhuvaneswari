75a57dbeae6fe05e80fbfb760839b0f8795f9c99
bhuvana <rbhuvanareddy@gmail.com>
2022-09-28 17:35:15 +0530

    upload

---
+++ 28sep/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BAL_Library;
+
+namespace sep28Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            studentoperations s = new studentoperations();
+           BLL_Class bal = new BLL_Class();
+        //    Console.WriteLine("Add student");
+        //    Console.WriteLine("enter studentid");
+
+        //    bal.studid = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("entr studname");
+        //    bal.name = Console.ReadLine();
+
+        //    Console.WriteLine("enter courseid");
+        //    bal.crsid = Convert.ToInt32(Console.ReadLine());
+
+        //    s.Insertstudent(bal);
+        //    Console.WriteLine("record Inserted successfully...");
+        //    Console.WriteLine("---------------------------------------------------------------------------");
+
+
+
+        //    Console.WriteLine("Update Student");
+        //    Console.WriteLine("enter studentid to update");
+        //    bal.studid = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("enter student name");
+        //    bal.name = Console.ReadLine();
+        //    Console.WriteLine("enter courseid");
+        //    bal.crsid = Convert.ToInt32(Console.ReadLine());
+        //    s.Updatestudent(bal);
+        //    Console.WriteLine("Updated successfully....");
+        //    Console.WriteLine("----------------------------");
+
+
+
+
+            Console.WriteLine("delete Student");
+            Console.WriteLine("enter studentid to delete");
+            bal.studid = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter student name");
+            //bal.name = Console.ReadLine();
+            //Console.WriteLine("enter courseid");
+            //bal.crsid = Convert.ToInt32(Console.ReadLine());
+            s.Deletestudent(bal);
+            Console.WriteLine("deleted successfully....");
+            Console.WriteLine("----------------------------");
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ 28sep/studentoperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_Library;
+
+namespace BAL_Library
+{
+    public class studentoperations
+    {
+        public void Insertstudent(BLL_Class bal)
+        {
+            InstituteManagementSystemEntities context = new InstituteManagementSystemEntities();
+            student s = new student();
+            s.studid = bal.studid;
+            s.name = bal.name;
+            s.crsid = bal.crsid;
+            context.students.Add(s);
+            context.SaveChanges();
+
+        }
+
+        public void Updatestudent(BLL_Class bal)
+        {
+            InstituteManagementSystemEntities context = new InstituteManagementSystemEntities();
+            List<student> students = context.students.ToList();
+            student obj = students.Find(student => student.studid == bal.studid);
+            obj.name = bal.name;
+            obj.crsid = bal.crsid;
+            context.SaveChanges();
+
+        }
+
+        public void Deletestudent(BLL_Class bal)
+        {
+            InstituteManagementSystemEntities context = new InstituteManagementSystemEntities();
+            List<student> students = context.students.ToList();
+            student obj = students.Find(student => student.studid == bal.studid);
+            //obj.name = bal.name;
+            //obj.crsid = bal.crsid;
+            context.SaveChanges();
+
+        }
+
+
+
+    }
+}
