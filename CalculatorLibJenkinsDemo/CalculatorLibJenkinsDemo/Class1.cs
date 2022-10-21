068be16f0422c1a93d6941538c35b34b064b9a2f
bhuvana <rbhuvanareddy@gmail.com>
2022-10-21 09:53:58 +0530

    upload

---
+++ CalculatorLibJenkinsDemo/CalculatorLibJenkinsDemo/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibJenkinsDemo
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 2;
+            int res = a + b;
+            Console.WriteLine(res);
+            Console.ReadLine();    
+        }
+       
+    }
+}
+;
